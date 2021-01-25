f0954aacb7a0c98599d7982cb451b7e4faa4918b
Amarvir <amarvirgill06@gmail.com>
2021-01-26 01:33:14 +0530

    userLOCation added

--- A1_iOS_Amarvir_c0783273/ViewController.swift
+++ A1_iOS_Amarvir_c0783273/ViewController.swift
@@ -7,14 +7,62 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var map: MKMapView!
+    
+    var locManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+        locManager.delegate = self
+        
+        locManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        locManager.requestWhenInUseAuthorization()
+    
+        locManager.startUpdatingLocation()
+        
     }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        
+        let latitude = location.coordinate.latitude
+        let longitude = location.coordinate.longitude
+        
+        displayLocation(lat: latitude, long: longitude, title: "You Are here")
+    }
+    
+    func displayLocation(lat: CLLocationDegrees,
+                         long: CLLocationDegrees,
+                         title:String){
+        
+        let latDelta:CLLocationDegrees = 0.010
+        let longDelta:CLLocationDegrees = 0.010
+        
+        let zoom = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+        
+        
+        let location = CLLocationCoordinate2D(latitude: lat, longitude: long)
+        
+        let region = MKCoordinateRegion(center: location, span: zoom)
+        
+        map.setRegion(region, animated: true)
+        
+        
+        let annotation = MKPointAnnotation()
+        annotation.title = title
+        annotation.coordinate = location
+        
+        map.addAnnotation(annotation)
+        
+    }
+    
 
 }
 
