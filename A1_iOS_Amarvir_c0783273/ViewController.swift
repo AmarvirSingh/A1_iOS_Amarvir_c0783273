0e79f7d428085a89c9a295cd9677d98980d3dbc5
Amarvir <amarvirgill06@gmail.com>
2021-01-26 02:07:47 +0530

    added markers (A,B,C)

--- A1_iOS_Amarvir_c0783273/ViewController.swift
+++ A1_iOS_Amarvir_c0783273/ViewController.swift
@@ -12,12 +12,16 @@ import MapKit
 class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var map: MKMapView!
+    var c = 0
     
     var locManager = CLLocationManager()
+    var nameArray = ["A","B","C"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        map.isZoomEnabled = false
     
         locManager.delegate = self
         
@@ -26,6 +30,35 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locManager.requestWhenInUseAuthorization()
     
         locManager.startUpdatingLocation()
+        
+        
+        let lp = UITapGestureRecognizer(target: self, action: #selector(longPress))
+        
+        map.addGestureRecognizer(lp)
+    }
+    
+    @objc  func longPress(gesture:UITapGestureRecognizer){
+                
+        let touch = gesture.location(in: map)
+        let coordinate = map.convert(touch, toCoordinateFrom: map)
+        gesture.numberOfTapsRequired = 2
+        let anno = MKPointAnnotation()
+         c += 1
+        print(c)
+        if c == 1{
+            anno.title = "A"
+            anno.coordinate = coordinate
+            map.addAnnotation(anno)
+        }else if c == 2{
+            anno.title = "B"
+            anno.coordinate = coordinate
+            map.addAnnotation(anno)
+        }else if c == 3 {
+            anno.title = "C"
+            anno.coordinate = coordinate
+            map.addAnnotation(anno)
+        }
+        
         
     }
 
